0629d1e883bf0d03f89d1a4ee51ece1fccc7c43d
Thomas Hidalgo <thomas.hidalgo@epitech.eu>
2021-03-27 16:59:00 +0100

    feat: finish adding map with points related to race's steps

--- run-list-app/src/components/AccordionItem.js
+++ run-list-app/src/components/AccordionItem.js
@@ -14,7 +14,7 @@ const AccordionItem = (item) => {
     return (
         <div>
             <h1>{name}</h1>
-            <h4>Description : {desc}</h4>
+            <h4>{desc}</h4>
         </div>
     )
 }
--- run-list-app/src/components/Map.js
+++ run-list-app/src/components/Map.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { LocationOn } from '@material-ui/icons';
+
+const LocationPin = (props) => (
+    <div>
+        <LocationOn style={{color: '#F97E3C'}}/>
+    </div>
+)
 
 const Map = ({list, center}) => {
-    const defaultCenter = [Math.round(parseFloat(center.lat) * 10) / 10, Math.round(parseFloat(center.lon) * 10) / 10];
 
     return(
         <div style={{ height: "60vh", width: "100%" }}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
                 defaultCenter={[43.6, 1.44]}
-                defaultZoom={13}
-            />
+                defaultZoom={14}
+            >
+                {list.map(({point}, index) => {
+                    let lat = parseFloat(point.point.lat);
+                    let lng = parseFloat(point.point.lon);
+
+                    return(
+
+                            <LocationPin
+                                key={index}
+                                lat={lat}
+                                lng={lng}
+                            />
+
+                    )
+                })}
+            </GoogleMapReact>
         </div>
     )
 }
